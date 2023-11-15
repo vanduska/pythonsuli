1705e61b3bf9c460aa977cdbef9f81b05bd0039d
Vandus <120109528+vanduska@users.noreply.github.com>
2023-11-15 08:39:37 +0100

    Add files via upload

---
+++ szovegRu.py
@@ -0,0 +1,9 @@
+text={  "Reggel felébredtem. Mit tegyek?":"Reggel felébredtem. Mit tegyek most?":"Я проснулся утром. Что мне делать?»: «Я проснулся утром. Что мне теперь делать?",
+        "fogmosás":"чистка зубов",
+        "reggeli":"завтрак",
+        "öltözés":"переодевание",
+        "66-os parancs":"Команда 66",
+        "Elmegyek fogatmosni. Sikálom rendesen, ahogy kell!":"Я собираюсь почистить зубы. Я кричу как следует, как надо!",
+        "Kellene valamit enni! Anya csinált valamit? Nézzük meg!":"Тебе следует что-нибудь съесть! Мама что-то сделала? Давайте посмотрим на!",
+        "Kissé hűvös van, Kelle valami ruha! Felveszek egy nadrágot, meg egy pólót!":"Немного прохладно, мне нужна одежда! Я надену штаны и футболку!"
+    }
